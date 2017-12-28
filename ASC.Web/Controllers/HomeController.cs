5e1be3969f17b8bf792ac0208fd9b3b3789793c9
Christian Traintinger <c.traintinger@gmx.net>
2017-12-28 12:06:14 +0100

    Initial Commit

---
+++ ASC.Web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASC.Web.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ASC.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IOptions<ApplicationSettings> _settings;
+
+        public HomeController(IOptions<ApplicationSettings> settings)
+        {
+            this._settings = settings;
+        }
+
+        public IActionResult Index()
+        {
+            // Usage of IOptions
+            ViewBag.Title = this._settings.Value.ApplicationTitle;
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
