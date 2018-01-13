aa4b90adbed7b706b8da49478f4aa7741c1d564e
Christian Traintinger <c.traintinger@gmx.net>
2018-01-13 19:01:55 +0100

    Chapter 9

---
+++ ASC.Web/Data/NavigationDataCache.cs
@@ -0,0 +1,40 @@
+﻿using ASC.Utilities.Navigation;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASC.Web.Data
+{
+    public interface INavigationCacheOperations
+    {
+        Task<NavigationMenu> GetNavigationCacheAsync();
+        Task CreateNavigationCacheAsync();
+    }
+
+    public class NavigationCacheOperations : INavigationCacheOperations
+    {
+        private readonly IDistributedCache _cache;
+        private readonly string NavigationCacheName = "NavigationCache";
+
+        public NavigationCacheOperations(IDistributedCache cache)
+        {
+            this._cache = cache;
+        }
+
+        public async Task CreateNavigationCacheAsync()
+        {
+            await this._cache.SetStringAsync(this.NavigationCacheName,
+                File.ReadAllText("Navigation/Navigation.json"));
+        }
+
+        public async Task<NavigationMenu> GetNavigationCacheAsync()
+        {
+            return JsonConvert.DeserializeObject<NavigationMenu>(await this._cache
+                .GetStringAsync(this.NavigationCacheName));
+        }
+    }
+}
